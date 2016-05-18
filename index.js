995963fdec636be98f2aacdaf91bea5c36110084
Guillaume Royer <perso@guilro.com>
2016-05-18 15:39:35 +0200

    add config for github

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const request = require('request-promise');
 const wrap = require('co-express');
 
 const config = require('./config');
@@ -18,6 +19,18 @@ if (app.get('env') === 'development') {
 }
 
 app.use(bodyParser.json());
+
+app.get('/status', wrap(function * (req, res) {
+  return res.send({
+    github: JSON.parse(yield request({
+      url: `https://api.github.com/rate_limit?client_id=${config.github.clientId}&client_secret=${config.github.clientSecret}`,
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'User-Agent': config.github.userAgent
+      }
+    }))
+  });
+}));
 
 var datastore = {};
 app.use('/list/:id', wrap(function * (req, res, next) {
